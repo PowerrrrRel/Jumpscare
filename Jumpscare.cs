fbafc9e6d068bb5f11eae0db87bc232bf978eae3
Powerrrr <156711620+PowerrrrRel@users.noreply.github.com>
2024-12-11 21:12:46 -0500

    Add files via upload

---
+++ Jumpscare.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+using System.Collections;
+
+public class Jumpscare : MonoBehaviour
+{
+    [Header("Player")]
+    public GameObject gorillaPlayer;
+    public new Rigidbody rigidbody;
+    public Transform playerTrans;
+    public new string tag;
+
+    [Header("Objects")]
+    public Transform respawnPoint;
+    public AudioSource audioSource;
+    public GameObject jumpscare;
+
+    [Header("Time")]
+    public float jumpscareTime;
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag(tag))
+        {
+            StartCoroutine(EnableJumpscare());
+        }
+    }
+
+    IEnumerator EnableJumpscare()
+    {
+        gorillaPlayer.GetComponent<Rigidbody>().isKinematic = true;
+        gorillaPlayer.GetComponent<Rigidbody>().useGravity = false;
+        gorillaPlayer.transform.position = respawnPoint.transform.position;
+        gorillaPlayer.transform.rotation = respawnPoint.transform.rotation;
+        audioSource.Play();
+        jumpscare.SetActive(true);
+
+        yield return new WaitForSeconds(jumpscareTime);
+
+        DisableJumpscare();
+    }
+
+    public void DisableJumpscare()
+    {
+        StopAllCoroutines();
+        gorillaPlayer.GetComponent<Rigidbody>().isKinematic = false;
+        gorillaPlayer.GetComponent<Rigidbody>().useGravity = true;
+        jumpscare.SetActive(false);
+        audioSource.Stop();
+    }
+}
+
+//This script was made by Powerrrr! Discord: PowerVacuum.
+//You don't have to give Credits but I would like credits :)
